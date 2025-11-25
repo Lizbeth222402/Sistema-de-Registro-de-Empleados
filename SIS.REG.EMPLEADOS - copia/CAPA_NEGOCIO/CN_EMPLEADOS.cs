6c15360c725eabc5da5cae22c313788236e69f72
Liz <josseling.gutierrez222402@udem.edu.ni>
2025-11-25 12:50:44 -0600

    Update CN_EMPLEADOS.cs
    Se realizo cambios en la CN DE EMPLEADOS

--- SIS.REG.EMPLEADOS - copia/CAPA_NEGOCIO/CN_EMPLEADOS.cs
+++ SIS.REG.EMPLEADOS - copia/CAPA_NEGOCIO/CN_EMPLEADOS.cs
@@ -3,12 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CAPA_DATO;
+using CAPA_ENTIDAD;
 
 namespace CAPA_NEGOCIO
 {
     public class CN_EMPLEADOS
     {
+        
+ CD_EMPLEADOS CD_empleados = new CD_EMPLEADOS();
 
+ //metodo para listar Departamento//
+
+ public List<CE_MEMPLEADOS> ListarEmpleados()
+
+ {
+     return CD_empleados.ListarEmpleados();
+ }
+
+ //Insertar///
+ public void InsertarEmpleados(CE_MEMPLEADOS cE_Empleados)
+ {
+     CD_empleados.InsertarEmpleados(cE_Empleados);
+
+ }
 
     }
 }
