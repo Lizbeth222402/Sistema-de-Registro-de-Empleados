ae0c8ae94dfe267276191a1d8d72e2cb46a25268
Auxiliar Operaciones Tracklink <auxoptrack@outlook.com>
2025-11-29 09:40:49 -0600

    commit

--- SIS.REG.EMPLEADOS - copia/NCAPA/FRM_NEW_USUARIO.cs
+++ SIS.REG.EMPLEADOS - copia/NCAPA/FRM_NEW_USUARIO.cs
@@ -7,11 +7,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using CAPA_ENTIDAD;
+using CAPA_NEGOCIO;
 
 namespace NCAPA
 {
     public partial class FRM_NEW_USUARIO : Form
     {
+        CE_MUSUARIOS Musuarios = new CE_MUSUARIOS();
+        CN_USUARIOS cN_USUARIOS = new CN_USUARIOS();
+
         public FRM_NEW_USUARIO()
         {
             InitializeComponent();
@@ -20,6 +25,32 @@ namespace NCAPA
         private void button1_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+
+        private void btnNuevoEmpleado_Click(object sender, EventArgs e)
+        {
+            GUARDARUSUARIO();
+        }
+
+
+        private void GUARDARUSUARIO ()
+        {
+            Musuarios.NOMBRE_USUARIO = txtNombre_usuario.Text.Trim();
+            Musuarios.CONTRASEÑA = txtpassUsuario.Text.Trim();
+            Musuarios.ACTIVO = txtUsuarioActivo.Text.Trim();
+
+
+            cN_USUARIOS.InsertarUsuario(Musuarios);
+
+        }
+
+        private void txtUsuarioActivo_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // Permite solo números y teclas de control (como Backspace)
+            if (!char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar))
+            {
+                e.Handled = true; // Bloquea la tecla
+            }
         }
     }
 }
